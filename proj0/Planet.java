814f75c3fbd80a98341005443760c63c89c4173e
Ranway <lxw823247061@gmail.com>
2022-03-22 01:19:43 +0800

    pro0 finished!

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,11 +1,11 @@
 
 public class Planet {
-    double xxPos;
-    double yyPos;
-    double xxVel;
-    double yyVel;
-    double mass;
-    String imgFileName;
+    private double xxPos;
+    private double yyPos;
+    private double xxVel;
+    private double yyVel;
+    private double mass;
+    private String imgFileName;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img){
         xxPos = xP;
