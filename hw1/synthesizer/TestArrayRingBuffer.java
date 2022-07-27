84799d355674a8f5f868139608d2abeed18d6cc3
Ranway <lxw823247061@gmail.com>
2022-07-27 17:02:12 +0800

    HW01,Task 3 finished.

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -1,6 +1,7 @@
 package synthesizer;
+import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 
 /** Tests the ArrayRingBuffer class.
  *  @author Josh Hug
@@ -8,8 +9,32 @@ import static org.junit.Assert.*;
 
 public class TestArrayRingBuffer {
     @Test
-    public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+    public void Test_Null() {
+        ArrayRingBuffer<Integer> arb= new ArrayRingBuffer<>(10);
+        Integer a = arb.dequeue();
+    }
+    @Test
+    public void Test_Full() {
+        ArrayRingBuffer<Integer> arb= new ArrayRingBuffer<>(3);
+        Assert.assertTrue(arb.isEmpty());
+        for(int i = 0 ; i < 3 ; i++) {
+            arb.enqueue(i);
+        }
+        Assert.assertTrue(arb.isFull());
+    }
+    @Test
+    public void Test_pop() {
+        ArrayRingBuffer<Integer> arb= new ArrayRingBuffer<>(10);
+        for(int i = 0 ; i < 5 ; i++) {
+            arb.enqueue(i);
+        }
+        Assert.assertEquals((Integer) 0,arb.dequeue());
+        arb.enqueue(55);
+        arb.dequeue();
+        arb.dequeue();
+        arb.dequeue();
+        arb.dequeue();
+        Assert.assertEquals((Integer) 55,arb.dequeue());
     }
 
     /** Calls tests for ArrayRingBuffer. */
